9adae7d05d50cbfdcf8c99d2b7e78d0481dfc825
yashwanth2108 <77567158+yashwanth2108@users.noreply.github.com>
2021-07-14 17:10:10 +0530

    Add files via upload

---
+++ corejava_Assignemnts/Q5/shape/java/Rectangle.java
@@ -0,0 +1,17 @@
+package shape.java;
+
+public class Rectangle extends shape {
+
+	
+	@Override
+	void draw() {
+		// TODO Auto-generated method stub
+		System.out.println("This is Rectangle");
+		System.out.println(" __________");
+		System.out.println("|          |");
+		System.out.println("|__________|");
+
+		
+	}
+
+}
