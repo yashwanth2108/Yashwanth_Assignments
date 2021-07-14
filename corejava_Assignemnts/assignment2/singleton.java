cb177d1fc11c262e6a61c9b70de085fd4afe4750
yashwanth2108 <77567158+yashwanth2108@users.noreply.github.com>
2021-07-14 17:00:02 +0530

    Add files via upload

---
+++ corejava_Assignemnts/assignment2/singleton.java
@@ -0,0 +1,16 @@
+package singleton.java;
+
+public final class singleton {
+	private singleton()
+	{
+		System.out.println("This is a singleton class");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		singleton s=new singleton();
+		
+
+	}
+
+}
