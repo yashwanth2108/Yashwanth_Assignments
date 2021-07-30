da7300cd12464c986203c162448a2fda3a3d9533
yashwanth2108 <77567158+yashwanth2108@users.noreply.github.com>
2021-07-30 15:32:37 +0530

    Update simpleIntrest.java

--- springmvc/q2/src/main/java/q2/simpleIntrest.java
+++ springmvc/q2/src/main/java/q2/simpleIntrest.java
@@ -1,5 +1,30 @@
 package q2;
 
-public class simpleIntrest {
+
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class simpleInterst{
+	
+	@RequestMapping("/calulate")
+	public ModelAndView add( HttpServletRequest request ,   HttpServletResponse response   )
+	{
+		double i = Integer.parseInt(request.getParameter("amount"));
+	    double j = Integer.parseInt(request.getParameter("years"));
+	    double k =  Integer.parseInt(request.getParameter("interest"));
+	    
+	    double L = (i*j*k)/100;
+	    ModelAndView mv = new ModelAndView();
+	    mv.setViewName("display.jsp");
+	    mv.addObject("result", L);
+	    
+	    return mv;
+	}
 
 }
